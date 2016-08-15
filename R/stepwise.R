0f44b9938e77c89ed38edc690326d3238a6eb58a
JustinCCYap <justinyapcc@gmail.com>
2016-08-15 15:44:45 +1000

    Added direction option to Stepwise, added unit tests

--- R/stepwise.R
+++ R/stepwise.R
@@ -2,11 +2,12 @@
 #' @description Stepwise selection of predictor variables by AIC.
 #' @param object a \code{Regression} object.
 #' @param output one of \code{Final}, \code{Detailed} or \code{All}, which determines how much information to display.
+#' @param direction one of \code{Backward} (backward elimination of variables) or \code{Forward} (forward selection of variables).
 #' @param always.include a vector of names of variables to always include in the model.
 #' @param steps the maximum number of steps to be considered.
 #' @importFrom MASS stepAIC
 #' @export
-Stepwise <- function(object, output = "Final", always.include = NULL, steps = 1000)
+Stepwise <- function(object, output = "Final", direction = "Backward", always.include = NULL, steps = 1000)
 {
     if (class(object) != "Regression")
         stop("Invalid regression model object supplied.")
@@ -30,17 +31,26 @@ Stepwise <- function(object, output = "Final", always.include = NULL, steps = 10
     always.include <- intersect(var.names, always.include)
     if (!is.null(always.include) && length(always.include) > 0)
     {
-        lower <- formula(paste(outcome.name, "~", paste(always.include, sep = "+")))
+        lower <- formula(paste(outcome.name, "~", paste(always.include, collapse = "+")))
         scope <- list(lower = lower, upper = object$formula)
     }
     else
-        scope <- NULL
+        scope <- object$formula
 
-    params <- c(list(formula = object$formula, data = object$estimation.data, weights = object$weights[object$subset],
-                     type = object$type, robust.se = object$robust.se), object$ellipsis)
+    reg.formula <- if (direction == "Backward")
+        object$formula
+    else if (!is.null(scope$lower))
+        scope$lower
+    else
+        formula(paste(outcome.name, "~ 1"))
+
+    params <- c(list(formula = reg.formula, data = object$estimation.data,
+                     weights = object$weights[object$subset], type = object$type,
+                     robust.se = object$robust.se), object$ellipsis)
     # Use do.call so that we can pass the ellipsis parameters
     reg.without.missing <- do.call("Regression", params)
-    selected.model <- stepAIC(reg.without.missing, scope = scope, trace = 0, steps = steps)
+    selected.model <- stepAIC(reg.without.missing, scope = scope, direction = tolower(direction),
+                              trace = 0, steps = steps)
 
     # Use the call from the original object but with an updated formula
     call.formula <- selected.model$call[attr(selected.model$call, "name") == "formula"]
@@ -53,7 +63,8 @@ Stepwise <- function(object, output = "Final", always.include = NULL, steps = 10
     if (output == "All")
     {
         # We assign the output to v so that only the steps are captured
-        captured <- capture.output(v <- stepAIC(reg.without.missing, scope = scope, steps = steps))
+        captured <- capture.output(v <- stepAIC(reg.without.missing, scope = scope,
+                                                direction = tolower(direction), steps = steps))
         result$steps.output <- paste(captured, collapse = "\n")
     }
 
