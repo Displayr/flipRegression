1b913ca1876d85d2422b29ddb8f91b8c9c830056
TimBock <tim.bock@numbers.net.au>
2016-09-15 14:46:40 +1000

    Making ConfusionMartix and S3 method

---
+++ tests/testthat/test-confusion.R
@@ -0,0 +1,28 @@
+context("Confusion")
+zformula <- formula("Overall ~ Fees + Interest + Phone + Branch + Online + ATM")
+data(bank, package = "flipExampleData")
+sb <- bank$ID > 100
+attr(sb, "label") <- "ID greater than 100"
+wgt <- bank$ID
+wgt[is.na(wgt)] = 0
+attr(wgt, "label") <- "ID"
+attr(bank$Overall, "label") <- "Overall satisfaction"
+attr(bank$Fees, "label") <- "Fees paid"
+attr(bank$Online, "label") <- "Online banking"
+type  = "Linear"
+bank$overalldiv3 = bank$Overall / 3
+
+test_that(paste("Confusion matrix for linear with non-integer dependent variables"),
+{
+    z = suppressWarnings(Regression(overalldiv3 ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, type = type, subset = sb, weights = wgt))
+    expect_error(ConfusionMatrix(z), NA)
+
+})
+
+
+for (type in c("Linear", "Poisson", "Quasi-Poisson","Binary Logit",  "NBD", "Multinomial Logit", "Ordered Logit"))
+   test_that(paste("Confusion matrix :", type),
+    {
+        z = suppressWarnings(Regression(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, type = type, subset = sb, weights = wgt))
+        expect_error(ConfusionMatrix(z), NA)
+})
