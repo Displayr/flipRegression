79db1149c206761043b575a8c560a173ede2cfa8
TimBock <tim.bock@numbers.net.au>
2016-11-17 14:59:29 +1100

    Tidying unit tets, so that bugs appears first

---
+++ tests/testthat/test-_bugs.R
@@ -0,0 +1,35 @@
+context("bugs")
+data(bank, package = "flipExampleData")
+zformula <- formula("Overall ~ Fees + Interest + Phone + Branch + Online + ATM")
+sb <- bank$ID > 100
+attr(sb, "label") <- "ID greater than 100"
+wgt <- bank$ID
+attr(wgt, "label") <- "ID"
+bank$o2 <- factor(unclass(bank$Overall) > 3)
+
+
+
+type = "Multinomial Logit"
+for(missing in c("Multiple imputation", "Imputation (replace missing values with estimates)", "Exclude cases with missing data"))
+        test_that(paste("DS-884 MNL with 2 category dependent variable", missing),
+      {
+          # no weight, no filter
+          z = suppressWarnings(Regression(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, missing = missing, data = bank, subset = TRUE,  weights = NULL, type = type))
+          z = suppressWarnings(Regression(o2 ~ Fees + Interest + Phone + Branch + Online + ATM, missing = missing, data = bank, subset = TRUE,  weights = NULL, type = type))
+          # weight
+          expect_error(suppressWarnings(Regression(o2 ~ Fees + Interest + Phone + Branch + Online + ATM, missing = missing, data = bank, subset = sb,  weights = NULL, type = type)), NA)
+          # weight, filter
+          expect_error(suppressWarnings(Regression(o2 ~ Fees + Interest + Phone + Branch + Online + ATM, missing = missing, data = bank, subset = TRUE,  weights = wgt, type = type)), NA)
+          # weight, filter
+          expect_error(z <- suppressWarnings(Regression(o2 ~ Fees + Interest + Phone + Branch + Online + ATM, missing = missing, data = bank, subset = sb,  weights = wgt, type = type)), NA)
+          expect_error(capture.output(suppressWarnings(print(z))),NA)
+      })
+
+
+test_that("Poisson ANOVA p-values are very different to Regression + ignore robust SE",
+{
+    # Removed support for robust se from regression 17 Nov 2016
+    expect_error(suppressWarnings(Regression(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank, type = "Poisson", robust.se = TRUE)))
+})
+
+
