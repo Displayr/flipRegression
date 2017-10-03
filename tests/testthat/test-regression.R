94be1dd61e253b87b59f4fa0a57d4a6097c1a2b8
Mathew W. McLean <mathew.w.mclean@gmail.com>
2017-10-03 16:37:22 +1100

    Fix broken unit test from change to ProcessQVariables
    * flipTransformations::ProcessQVariables is now careful about
    preserving row.names; including integer values
    
    * The change to ProcessQVariables affected a flipRegression unit test and
    caused all.equal to error because the Regression call produces data data.frames
    with integer row.names (1:nrow(data)) whereas the call involving lm
    has data with row.names as.character(1:nrow(data))
    
    Addresses: DS-1471

--- tests/testthat/test-regression.R
+++ tests/testthat/test-regression.R
@@ -91,7 +91,7 @@ test_that("allEffects works on Regression object",
     data(cpus, package = "MASS")
     z <- Regression(log10(perf) ~ syct+mmin+mmax+cach+chmin+chmax, data = cpus)
     zlm <- lm(log10(perf) ~ syct+mmin+mmax+cach+chmin+chmax, data = cpus)
-    expect_equal(effects::allEffects(z), effects::allEffects(zlm))
+    expect_equal(effects::allEffects(z), effects::allEffects(zlm), check.attributes = FALSE)
 })
 
 for(missing in c("Multiple imputation", "Imputation (replace missing values with estimates)", "Exclude cases with missing data"))
