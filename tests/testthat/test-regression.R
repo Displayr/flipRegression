59264bdbde499cc883b9fd108ae4eeafbdeb87c1
Justin Yap <JustinCCYap@users.noreply.github.com>
2020-01-14 15:07:18 +1100

    Add unit test

--- tests/testthat/test-regression.R
+++ tests/testthat/test-regression.R
@@ -256,3 +256,8 @@ test_that("Error if too many dummy predictors",
                            "There are fewer observations.")
           })
 
+test_that("Ensure output size does not get too large (DS-2518)", {
+              bank50 <- do.call("rbind", lapply(1:50, function(x) bank))
+              result <- Regression(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank50)
+              expect_true(as.numeric(object.size(result)) < 10000000) # less than 10MB
+          })
