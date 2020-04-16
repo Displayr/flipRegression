30d8c2583a679987e6a0863a4a5b8dc169714868
Justin Yap <JustinCCYap@users.noreply.github.com>
2020-04-16 13:45:02 +1000

    DS-2702 Preserve label attribute (#6) [revdep skip]
    * Preserve label attribute
    
    * Changes to work with pysch 1.9.12 and mice 3.7.0
    
    * More changes to deal with pysch and mice package updates
    
    * Update tests for package updates
    
    * OS-specific test outputs for imputation
    
    * Update references
    
    * Fix unit test
    
    * Update NAMESPACE
    
    * Rely on AsNumeric to preserve attributes
    
    * Use DS-2702 branch of flipTransformations
    
    * Use DS-2702 branch of flipU
    
    * Change order of flipU and flipTransformations in travis.yml
    
    * DS-2704 Add documentation for robust se singularity adjustment [ci skip]
    
    * DS-2836 Update documentation for stacking [ci skip]
    
    Co-authored-by: chschan <chschan@users.noreply.github.com>
    Co-authored-by: Justin Yap <justinyapcc@gmail.com>
    Co-authored-by: jrwishart <justin.r.wishart@gmail.com>

--- tests/testthat/test-regression.R
+++ tests/testthat/test-regression.R
@@ -277,8 +277,18 @@ test_that("Error if too many dummy predictors",
                            "There are fewer observations.")
           })
 
+test_that("DS-2702: Outcome labels are preserved", {
+    bank$Overall <- as.factor(bank$Overall)
+    bank$Overall[is.na(bank$Overall)] <- 1 # previously label was lost for variables without missing values
+    attr(bank$Overall, "label") <- "lbl"
+    result <- suppressWarnings(Regression(Overall ~ Fees + Interest + Phone + Branch + Online + ATM,
+                                          data = bank, show.labels = TRUE))
+    expect_equal(result$outcome.label, "lbl")
+})
+
 test_that("Ensure output size does not get too large (DS-2518)", {
               bank50 <- do.call("rbind", lapply(1:50, function(x) bank))
               result <- suppressWarnings(Regression(Overall ~ Fees + Interest + Phone + Branch + Online + ATM, data = bank50))
               expect_true(as.numeric(object.size(result)) < 10000000) # less than 10MB
           })
+
