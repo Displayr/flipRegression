065713b995d05e21946674d6881a1e1ad67b3d65
Mathew W. McLean <mathew.w.mclean@gmail.com>
2021-05-26 18:30:20 +1000

    Remove names from residuals and fitted values
    * Remove fillInMissingRowNames in variables.R as it's not
    clear what this actually does and unnaming the residuals
    and fitted values saves size
    * DS-3339

--- R/variables.R
+++ R/variables.R
@@ -24,7 +24,8 @@ residuals.Regression <- function(object, type = "raw", ...)
         return(observed - predicted)
     }
     resids <- residuals(object$original, ...)
-    fillInMissingRowNames(rownames(object$model), resids)
+    ## fillInMissingRowNames(rownames(object$model), resids)
+    return(resids)
 }
 
 
@@ -144,7 +145,8 @@ fitted.Regression <- function(object, ...)
     notValidForPartial(object, "fitted")
     notValidForCrosstabInteraction(object, "fitted")
     fitted.values <- fitted(object$original)
-    fillInMissingRowNames(rownames(object$model), fitted.values)
+    ## fillInMissingRowNames(rownames(object$model), fitted.values)
+    return(fitted.values)
 }
 
 fillInMissingRowNames <- function(row.names, variable)
