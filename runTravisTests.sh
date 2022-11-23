723937dc2c750a42bdaedf4a17124ebcd58d9c63
Mathew W. McLean <mathew.w.mclean@gmail.com>
2022-11-23 11:49:24 +1100

    Update travis script to fix notification [revdep skip]

--- runTravisTests.sh
+++ runTravisTests.sh
@@ -11,8 +11,8 @@ if [ -d tests/testthat ]; then
     echo "tmp <- unlist(strsplit(split='[[:space:]]+', out[line.fail]))" >> test.R
     echo "pos.fail <- grep('FAIL', tmp)" >> test.R
     echo "n.fail <- as.numeric(tmp[pos.fail+1])" >> test.R
-    echo "res <- as.data.frame(res); out <- data.frame(file = unlist(res[['file']]), warning = unlist(res[['warning']]))" >> test.R 
-    echo "write.csv(out, file='test_results.csv')" >> test.R
+    echo "res <- as.data.frame(res); out <- data.frame(fail = res[['failed']] + res[['error']], file = unlist(res[['file']]), warning = unlist(res[['warning']]))" >> test.R 
+    echo "write.csv(out, file='test_results.csv', row.names = FALSE)" >> test.R
     echo "quit(status = !identical(n.fail, 0), save='no')" >> test.R
     Rscript --default-packages="datasets,utils,grDevices,graphics,stats,methods" test.R
     exit $?
