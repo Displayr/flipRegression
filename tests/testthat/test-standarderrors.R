87a85dc31cf333f3e9942788536acd36a06b8f9f
jrwishart <justin.wishart@displayr.com>
2023-03-21 13:53:09 +1100

    DS-4060 Improve unit tests of robust standard errors

--- tests/testthat/test-standarderrors.R
+++ tests/testthat/test-standarderrors.R
@@ -10,7 +10,7 @@ colas$balanced <- c(rep(1:3, rep(100,3)), rep(NA, 27))
 colas$agenumeric <- car::recode(colas$d1, as.factor = FALSE, recodes = "'18 to 24' = 21; '25 to 29' = 27; '30 to 34' = 32; '35 to 39' = 37; '40 to 44' = 42; '45 to 49' = 47; '50 to 54' = 52; '55 to 64' = 60; '65 or more' = 77")
 
 test_that("Stata Linearized Standard Errors with weights", {
-    z = Regression(like.coke~d1, weights = colas$agenumeric, data = colas, robust.se = FALSE)
+    z <- Regression(like.coke~d1, weights = colas$agenumeric, data = colas, robust.se = FALSE)
     # se
     expect_equal(as.numeric(z$summary$coefficients[1:3, 2]), c( .1186729, .2204833, .1738284), tolerance = 0.0000001)
     # p
@@ -18,8 +18,7 @@ test_that("Stata Linearized Standard Errors with weights", {
 })
 
 test_that("Stata Robust Standard Errors", {
-    z = suppressWarnings(Regression(like.coke~d1, data = colas, robust.se = "hc1", show.labels = TRUE))
-    #Regression(like.coke~d1, data = colas, robust.se = "hc1", show.labels = TRYE)
+    z <- Regression(like.coke~d1, data = colas, robust.se = "hc1", show.labels = TRUE)
     # xtabs(~d1 + like.coke, data = colas)
     # data.frame(colas$d1, colas$like.coke)
     # table(colas$d1)
@@ -31,3 +30,32 @@ test_that("Stata Robust Standard Errors", {
     expect_equal(as.numeric(z$summary$coefficients[1:4, 4]), c( 0.000, 0.127 , 0.667 , 0.593), tolerance = 0.0005)
 })
 
+test_that("HCCM calculations", {
+    # Use the PublicSchool data from the sandwich package
+    # Compre the generated values against the values in the sandwich package
+    data(PublicSchools, package = "sandwich")
+    quad.model <- lm(Expenditure ~ Income + I(Income^2), data = PublicSchools)
+    technical.problem <- "There is a technical problem with the parameter variance-covariance matrix"
+    adjustments <- paste0("hc", 0:4)
+    for (type in adjustments) {
+        expect_error(car::hccm(quad.model, type = "hc0"), "object 'bads' not found")
+        expect_warning(output <- vcov2(quad.model, robust.se = type), technical.problem)
+        expect_equal(output, sandwich::vcovHC(quad.model, type = toupper(type)))
+    }
+    # No robust adjustment
+    expect_equal(vcov2(quad.model, robust.se = FALSE), vcov(quad.model))
+    # Check a model with no issues, taken from sandwich::vcovHC help page
+    set.seed(20230321)
+    x <- sin(1:100)
+    y <- 1 + x + rnorm(100)
+    weights <- runif(100)
+    dat <- data.frame(x, y, weights)
+    ## model fit and HC3 covariance
+    model <- lm(y ~ x, data = dat)
+    design <- survey::svydesign(ids = ~1, weights = ~weights, data = dat)
+    weighted.model <- survey::svyglm(y ~ x, design)
+    expect_equal(vcov2(weighted.model), FixVarianceCovarianceMatrix(vcov(weighted.model)))
+    # Check other robust adjustments
+    for (type in adjustments)
+        expect_equal(vcov2(model, robust.se = type), sandwich::vcovHC(model, type = toupper(type)))
+})
