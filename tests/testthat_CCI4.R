aa1cac3a69c9c731b105cfc79eba314c6c57f06b
Mathew W. McLean <mathew.w.mclean@gmail.com>
2023-05-30 16:18:20 +1000

    DS-4695: Add test files to split tests by filename (#71)

---
+++ tests/testthat_CCI4.R
@@ -0,0 +1,14 @@
+if (identical(Sys.getenv("CIRCLECI"), "true"))
+{
+    test.files <- list.files("tests/testthat", pattern = "\\.R$")
+    test.files <- gsub("test-|\\.R$", "", test.files)
+    test.filter <- grep("^[asrcd]", test.files,
+                        invert = TRUE, value = TRUE)
+    test.filter <- paste0("^", test.filter, "$")
+    if (!dir.exists("reports"))
+        dir.create("reports")
+    out.file <- paste0("reports/test_results", Sys.getenv("CIRCLE_NODE_INDEX"), ".xml")
+    exit.code <- flipDevTools::RunTestsOnCircleCI(filter = paste0(test.filter, collapse = "|"),
+                                                  load_package = "none", output_file = out.file)
+    q(status = exit.code, save = "no")
+}
