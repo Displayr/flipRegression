2a46018193b782df2df6a7f8beb7316c47f012fc
jrwishart <justin.wishart@displayr.com>
2025-01-03 16:14:11 +1100

    Untracked: Change test settings in CircleCI

--- tests/testthat_CCI3.R
+++ tests/testthat_CCI3.R
@@ -6,7 +6,6 @@ if (identical(Sys.getenv("CIRCLECI"), "true"))
     if (!dir.exists("reports"))
         dir.create("reports")
     out.file <- paste0("reports/test_results", Sys.getenv("CIRCLE_NODE_INDEX"), ".xml")
-    exit.code <- flipDevTools::RunTestsOnCircleCI(filter = "^[cd]",
-                                                  load_package = "none", output_file = out.file)
+    exit.code <- flipDevTools::RunTestsOnCircleCI(path = ".", filter = "^[cd]", output_file = out.file)
     q(status = exit.code, save = "no")
 }
